3b4b28e864fd00a80d36996b4814647dcfd7aa0c
Elina <r-elina-r@yandex.ru>
2023-02-15 02:13:01 +0300

    fix main

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Eelllllyy/portfolio.git main:gh-pages
+git push -f git@github.com:Eelllllyy/portfolio.git master:gh-pages
 cd -
